f813f6834cf3119daabed9343d6189b1a1e4bf52
Kimbangg <ehehdgus1@hanyang.ac.kr>
2021-02-10 15:29:43 +0900

    # 지역별 페이지 UI 완료

--- travel-diary-front/src/App.js
+++ travel-diary-front/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import LoginPage from './Components/Login/LoginPage';
 import MainPage from './Components/MainPage';
-import CollectionMain from './Components/Collection/CollectMain';
+import CollectMain from './Components/Collection/CollectMain';
 import { BrowserRouter, Route } from 'react-router-dom';
 import GoogleButton from './Components/Login/GoogleButton';
+import CollectDetail from './Components/Collection/CollectDetail';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -19,7 +20,8 @@ function App() {
         <GlobalStyle />
         <Route path="/" component={LoginPage} exact />
         <Route exact path="/mainpage" component={MainPage} />
-        <Route exact path="/collection" component={CollectionMain} />
+        <Route exact path="/collection" component={CollectMain} />
+        <Route exact path="/collection/detail" component={CollectDetail} />
       </BrowserRouter>
     </>
   );
