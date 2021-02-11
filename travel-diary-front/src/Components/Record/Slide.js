1ba24dac4375e60547b503f468c2e2534e7e1d03
hyehyes5 <hyeseung2000@gmail.com>
2021-02-11 15:10:33 +0900

    Update ViewRecord

--- travel-diary-front/src/Components/Record/Slide.js
+++ travel-diary-front/src/Components/Record/Slide.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const IMG = styled.img`
     width: 100%;
-    height: 30vh;
+    height: 40vh;
 `;
 
 function Slide({ img }) {
--- travel-diary-front/src/Components/Record/ViewRecord.js
+++ travel-diary-front/src/Components/Record/ViewRecord.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import ImageSlider from './ImageSlider';
 import styled from 'styled-components';
 import Write from '../../images/Write.png';
+import Location from '../../images/Location.png';
 import RecordDialog from './RecordDialog';
 
 const MainPageBlock = styled.div`
@@ -23,13 +24,13 @@ const HeadButtonBlock = styled.div`
   button.back {
     width: 30px;
     height: 30px;
-    margin: 2% 0% 0% 4%;
+    margin: 2% 0 0 4%;
     cursor: pointer;
   }
   button.edit {
     width: 30px;
     height: 30px;
-    margin: 2% 0% 0% 62%;
+    margin: 2% 0 0 62%;
     background-color: white;
     border: 0;
     outline: none;
@@ -42,30 +43,37 @@ const HeadButtonBlock = styled.div`
   button.delete {
     width: 30px;
     height: 30px;
-    margin: 2% 0% 0% 4%;
+    margin: 2% 0 0 4%;
     cursor: pointer;
   }
 `;
 
 const RecordBlock = styled.div`
-  margin: 7% 0% 0% 0%;
-  h5.date{
-    text-align: center;
-    font-family:'Noto Sans KR';
-    color: gray;
-  }
+  margin: 10% 0 0 0;
+  font-family:'Noto Sans KR';
+  text-align: center;
   h2 {
-    text-align: center;
-    font-family:'Noto Sans KR';
+    margin: 0;
+    padding: 0;
   }
-  h5.location{
-    text-align: center;
-    font-family:'Noto Sans KR';
+  h5.date{
+    color: gray;
+    margin: 10% 0 2% 0;
+    padding: 0;
+    font-weight: 500;
+  }
+  p.location{
+    font-size: 13px;
+    margin: 2% 0 10% 0;
+    img {
+      width: 5%;
+      margin: 3% 1% 0 0;
+    }
     color: #6fcf97;
+    font-weight: 500;
   }
   h5.content {
-    text-align: center;
-    font-family:'Noto Sans KR';
+    font-weight: 500;
   }
   hr {
     width: 300px;
@@ -111,7 +119,7 @@ function Record() {
           <RecordBlock>
             <h5 className="date">2021년 2월 10일</h5>
             <h2>광화문에서</h2>
-            <h5 className="location">서울특별시 종로구</h5>
+            <p className="location"><img alt="location" src={Location} />서울특별시 종로구</p>
             <hr />
             <h5 className="content">어쩌구 저쩌구</h5>
           </RecordBlock>
